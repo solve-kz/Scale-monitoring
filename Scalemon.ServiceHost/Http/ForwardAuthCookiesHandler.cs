1fc20ff81e27487d2c6d2dd18aed21c89c30c029
chimkentec <chimkentec@gmail.com>
2025-10-07 08:02:45 +0500

    Fix cookie forwarding handler

--- Scalemon.ServiceHost/Http/ForwardAuthCookiesHandler.cs
+++ Scalemon.ServiceHost/Http/ForwardAuthCookiesHandler.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Primitives;
 using Microsoft.Net.Http.Headers;
+using System.Linq;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
@@ -18,23 +18,22 @@ public class ForwardAuthCookiesHandler : DelegatingHandler
 
     protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
     {
-        var cookieHeader = _httpContextAccessor.HttpContext?.Request?.Headers[HeaderNames.Cookie];
+        var httpContext = _httpContextAccessor.HttpContext;
 
-        if (!StringValues.IsNullOrEmpty(cookieHeader))
+        if (httpContext?.Request is not { Cookies.Count: > 0 } requestContext)
+        {
+            return base.SendAsync(request, cancellationToken);
+        }
+
+        var cookiePairs = requestContext.Cookies
+            .Select(cookie => $"{cookie.Key}={cookie.Value}")
+            .Where(pair => !string.IsNullOrWhiteSpace(pair))
+            .ToArray();
+
+        if (cookiePairs.Length > 0)
         {
             request.Headers.Remove(HeaderNames.Cookie);
-
-            foreach (var value in cookieHeader.ToArray())
-            {
-                if (CookieHeaderValue.TryParse(value, out var cookie))
-                {
-                    request.Headers.TryAddWithoutValidation(HeaderNames.Cookie, cookie.ToString());
-                }
-                else
-                {
-                    request.Headers.TryAddWithoutValidation(HeaderNames.Cookie, value);
-                }
-            }
+            request.Headers.TryAddWithoutValidation(HeaderNames.Cookie, string.Join("; ", cookiePairs));
         }
 
         return base.SendAsync(request, cancellationToken);
