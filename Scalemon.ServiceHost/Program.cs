f00201a419a2fbf9e7c8e3044a7293021b486efa
chimkentec <chimkentec@gmail.com>
2025-10-09 16:02:27 +0500

    Require authentication for API and UI endpoints

--- Scalemon.ServiceHost/Program.cs
+++ Scalemon.ServiceHost/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -194,7 +195,12 @@ builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationSc
             return Task.CompletedTask;
         };
     });
-builder.Services.AddAuthorization();
+builder.Services.AddAuthorization(options =>
+{
+    options.FallbackPolicy = new AuthorizationPolicyBuilder()
+        .RequireAuthenticatedUser()
+        .Build();
+});
 
 // Сервисы для Blazor и Radzen UI
 builder.Services.AddRazorComponents().AddInteractiveServerComponents();
@@ -273,10 +279,12 @@ app.UseAuthorization();
 app.UseAntiforgery();
 
 // Конечные точки (API и Blazor UI)
-app.MapControllers();
+var apiEndpoints = app.MapControllers();
+apiEndpoints.RequireAuthorization();
 
-app.MapRazorComponents<App>()
-   .AddInteractiveServerRenderMode();
+var razorComponents = app.MapRazorComponents<App>();
+razorComponents.AddInteractiveServerRenderMode();
+razorComponents.RequireAuthorization();
 
 
 // --- 5. ЗАПУСК ПРИЛОЖЕНИЯ ---
